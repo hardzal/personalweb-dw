6932cd7ab06a2bfde95d5cf40ce293c7ef9372e7
hardzal <suryadijogja@gmail.com>
2025-01-10 22:36:35 +0700

    fix testimonial api

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,11 @@
-const db = require("../config/connect.js");
+const { Sequelize, QueryTypes } = require("sequelize");
+const config = require("../config/config.json");
+const db = new Sequelize(config.production);
 
 async function blogList(req, res) {
   try {
     const result = await db.query(`SELECT * FROM public."Users"`);
-    return res.json(result.rows);
+    return res.json(result[0]);
   } catch (err) {
     console.error(err);
     return res.status(500).send("Internal Server Error");
@@ -14,7 +16,7 @@ async function getTestimonials(req, res) {
   try {
     const result = await db.query(`SELECT * FROM public."Testimonials"`);
 
-    return res.json(result.rows);
+    return res.json(result[0]);
   } catch (error) {
     console.error(error);
     return res.status(500).send("Internal Server Error");
